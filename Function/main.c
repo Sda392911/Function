760740d15e3092ee8abd73b87efeec6cc8c4c613
Yvan Gauthier <ygauthier06@icloud.com>
2025-02-13 11:16:41 +0100

    Initial Commit

---
+++ Function/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Function
+//
+//  Created by Yvan Gauthier on 13/02/2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
