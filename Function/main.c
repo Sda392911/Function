32c169eda7517f3dc770368f0971bf021cff9457
Yvan Gauthier <ygauthier06@icloud.com>
2025-02-15 21:04:12 +0100

    Initial commit

--- Function/main.c
+++ Function/main.c
@@ -7,8 +7,28 @@
 
 #include <stdio.h>
 
+
+// This is program in a simple fonction which is part of my training to
+// Well understand the C language and eductate myself.
+
+int FirstFunction(void) {
+    // This is where the first function will be written.
+    // The functon output in a integer.
+    int age;
+    
+    printf("How old are you ? ");
+    scanf("%d", &age);
+    printf("You are %d\n", age);
+    return 0;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    // Simple condisonal statement.
+    if ( FirstFunction() <= 18) {
+        printf("You can do that\n");
+    } else {
+        printf("You cannot do that just yet\n");
+    }
     return 0;
 }
